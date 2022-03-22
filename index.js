696268c4a037dd8253e7366058185689663bb83a
Isabella Costabile <costabileisa@gmail.com>
2022-03-22 13:50:07 -0400

    commit tree ca381d221b803ae2509dc4362f10da28457bcabe

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementById("main").remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Isa is the champion!";
+document.body.append(newHeader);
